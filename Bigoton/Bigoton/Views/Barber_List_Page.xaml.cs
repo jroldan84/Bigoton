8f270004297427af1ebfc0c2465e6e8cd5f3875e
jroldan84 <jroldan84@hotmail.com>
2019-11-02 22:12:36 -0500

    se crea  login.

---
+++ Bigoton/Bigoton/Views/Barber_List_Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Bigoton.Views
+{
+    public partial class Barber_List_Page : ContentPage
+    {
+        public Barber_List_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
