9bdac5e6d88f3773032aad049c383e674528fa0b
jroldan84 <jroldan84@hotmail.com>
2019-11-04 18:04:59 -0500

    create the conexion of database

---
+++ Bigoton.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Bigoton.Web.Data.Entities;
+
+namespace Bigoton.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Client> Clients { get; set; }
+    }
+}
---
+++ Bigoton.Web/Data/Entities/Client.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Bigoton.Web.Data.Entities
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        
+
+        [Display(Name = "Document")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Document { get; set; }
+
+        [Display(Name = "First Name")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Cell Phone")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string CellPhone { get; set; }
+
+        [Display(Name = "Birth Date")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public double BirthDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName => $"{FirstName} {LastName}";
+
+        [Display(Name = "Full Name")]
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+
+    }
+}
