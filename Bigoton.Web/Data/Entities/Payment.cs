77260cebf5f80ad37803e18189e925c93d44a646
jroldan84 <jroldan84@hotmail.com>
2019-11-09 22:36:48 -0500

    create  the table  Payment

---
+++ Bigoton.Web/Data/Entities/Payment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bigoton.Web.Data.Entities
+{
+    public class Payment
+    {
+
+        public int Id { get; set; }
+
+
+        [Display(Name = "Total Payment")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public float TotalPayment { get; set; }
+
+        [Display(Name = "Date")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+
+
+
+        public Client Client { get; set; }
+
+    }
+}
