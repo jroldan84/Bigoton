d19aeeb1d9bfdb8b4d52ecf6b007cdc753fb3ec8
jroldan84 <jroldan84@hotmail.com>
2019-11-09 21:36:46 -0500

    create the  table  Employee

---
+++ Bigoton.Web/Data/Entities/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bigoton.Web.Data.Entities
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+
+        [Display(Name = "Name")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Name { get; set; }
+
+        public Reservation Reservation { get; set; }
+
+    }
+}
